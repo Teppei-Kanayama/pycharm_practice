5460ec061ed7682fb0bdf7cff5ef1fac95045d92
Teppei Kanayama <teppei@Kanayama-2.local>
2019-05-11 15:41:41 +0900

    first commit

---
+++ dummy_file.py
@@ -0,0 +1,10 @@
+from pycharm_practice import func
+
+x = np.asarray((10, 10))
+
+print(func(x))
+
+# タブの切り替え
+# ctr + →
+# タブを閉じる
+# Command + F4
---
+++ pycharm_practice.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+# 行頭・行末への移動
+# function + 矢印
+# もしくは Home, End
+x = np.asarray((10, 10))
+
+# 行全体の削除
+# Command + Y
+print('delete this line')
+
+# コードブロックの拡大・縮小
+# Command + W ／ Command + Shift + W
+for i in range(10):
+    print(f'code block {i}')
+
+# 行全体の削除
+# Command + Y
+# 行全体のコピー
+# コピー履歴からの貼り付け
+# Command + Shift + V
+print('copy 1')
+print('copy 2')
+print('copy 1')
+
+# 基本補完
+# ctr + space
+z = np.arr
+
+# 最近開いたファイルの表示
+# Command + E
+
+# タブの切り替え
+# ctr + →
+
+
+def func(arr):
+    return arr
+
+# somewhere -> terminal
+# ctr tab t
+
+# somewhere -> editor
+# esc
+
+# 横断テキスト検索
+# Ctrl + Shift + F
+
+# ファイル検索
+# Shift Shift
+
+# 該当箇所をフォーマットする
+# option + enter
+for i in range(10):
+    print(i+1)
+
+# import文を補う
+# これもoption + enterでできる
+m = pandas.Dataframe()
+
+print(func(x))
+
+# 変数が使われている箇所にジャンプ
+hoge = 'hogehoge'
+fuga = hoge * 2
+piyo = hoge * 3
+
+print(hoge)
